a2602b17ac56afdf56a6e36611d5e7dc01aa8b20
IgorBezusenko <bezusenko.98@mail.ru>
2020-09-17 22:25:14 +0300

    Updates

---
+++ precache-manifest.a3be8308dd7f6f0531266d10bcb23025.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f366e53eb43ca50dc8e734532fbeceb4",
+    "url": "/Nasa-API/index.html"
+  },
+  {
+    "revision": "abb9a6c385bdc54f106a",
+    "url": "/Nasa-API/static/js/2.4a08d7fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Nasa-API/static/js/2.4a08d7fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e5d7cb79ed4bbfd9f7",
+    "url": "/Nasa-API/static/js/main.439a729a.chunk.js"
+  },
+  {
+    "revision": "7237cfae78058c056b06",
+    "url": "/Nasa-API/static/js/runtime-main.c37d4dcd.js"
+  }
+]);
